24cb5e66aea32440094dc5f9d8b5577a49bac450
Arioo Barzan <arioobarzan@hotmail.com>
2023-11-08 10:02:17 +0330

    Create customers.cs

---
+++ WindowsFormsApp14/WindowsFormsApp14/customers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class customers : Form
+    {
+        public customers()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox1.Text;
+                string family = textBox2.Text;
+                string tel = textBox3.Text;
+                string line = name + "," + family + "," + tel + "\n";
+                File.AppendAllText("sample.csv", line);
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
