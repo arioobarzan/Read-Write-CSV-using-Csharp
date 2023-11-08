4cd75263513b65b14d3e24cad7d6e8d45d9f0969
Arioo Barzan <arioobarzan@hotmail.com>
2023-11-08 10:02:09 +0330

    Create Form1.cs

---
+++ WindowsFormsApp14/WindowsFormsApp14/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        Factorial fa = new Factorial();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            fa.Show();
+            fa.SetText2(textBox1.Text);
+
+
+            //MessageBox.Show(textBox1.Text.);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            customers cs = new customers();
+            cs.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowCustomers sc = new ShowCustomers();
+            sc.Show();
+        }
+    }
+}
