c257c35cd8babf533fe256af4ef51d7aac7955fd
Arioo Barzan <arioobarzan@hotmail.com>
2023-11-08 10:02:47 +0330

    Create ShowCustomers.cs

---
+++ WindowsFormsApp14/WindowsFormsApp14/ShowCustomers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class ShowCustomers : Form
+    {
+        public ShowCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowCustomers_Load(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("sample.csv");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                listBox2.Items.Add(lines[i]);
+            }
+
+        }
+    }
+}
