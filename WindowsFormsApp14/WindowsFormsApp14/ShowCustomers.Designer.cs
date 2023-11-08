56fd01b409db3d3b1ba197d91014e66024399bae
Arioo Barzan <arioobarzan@hotmail.com>
2023-11-08 10:02:44 +0330

    Create ShowCustomers.Designer.cs

---
+++ WindowsFormsApp14/WindowsFormsApp14/ShowCustomers.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WindowsFormsApp14
+{
+    partial class ShowCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(223, 60);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(812, 654);
+            this.listBox2.TabIndex = 1;
+            // 
+            // ShowCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1294, 747);
+            this.Controls.Add(this.listBox2);
+            this.Name = "ShowCustomers";
+            this.Text = "ShowCustomers";
+            this.Load += new System.EventHandler(this.ShowCustomers_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
