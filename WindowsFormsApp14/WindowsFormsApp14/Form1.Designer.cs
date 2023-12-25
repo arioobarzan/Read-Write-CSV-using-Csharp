72042dcf608d874d4da83bb3b3fb1bc5687d5391
Arioo Barzan <arioobarzan@hotmail.com>
2023-12-25 16:35:01 +0330

    Update Form1.Designer.cs

--- WindowsFormsApp14/WindowsFormsApp14/Form1.Designer.cs
+++ WindowsFormsApp14/WindowsFormsApp14/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace WindowsFormsApp14
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -72,11 +74,33 @@ namespace WindowsFormsApp14
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(68, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 204);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "ۀیست گوشی";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(68, 335);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(238, 130);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "اضافه کردن گوشی";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1407, 575);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -94,6 +118,8 @@ namespace WindowsFormsApp14
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
